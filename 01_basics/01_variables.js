9ca893c3b0c23408a60920cbde75d61f95b5c7ac
Malaika Saba <132433088+Malaikasaba@users.noreply.github.com>
2024-01-16 10:43:53 +0000

    conversionon operations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+let accountEmail = "saba@gmail.com";
+var accountPassword = "4567";
+accountCity = "Gilgit";
+let accountState;
+//  accountId = 234 not allowed
+
+accountEmail = "malaika@gmail.com";
+accountPassword = "8329";
+accountCity = "Pindi"
+
+// console.log(accountId);
+/* prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.log([accountId, accountEmail,accountPassword, accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; /*Treat all code as newer version*/
+
+// alert(3 + 3); we are using nodejs, not browser
+
+let name = "Malaika Saba"; /*string DataType*/
+let age = 20;   /*Number DataType*/
+let isLoggedIn = false;     /*Boolean DataType*/
+
+// number =>  2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not definedor declared
+// symbol => unique
+// object
+console.log(typeof "Malaika Saba");
+console.log(typeof undefined); /*undefined*/
+console.log(typeof null); /* object */
